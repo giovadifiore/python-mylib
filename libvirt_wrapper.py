63ba29cdcb50df0501f5ac13fd8a7577d6e29feb
Giovanni Di Fiore <giovannidifiore@MacBook-Pro-di-Giovanni.local>
2016-01-22 09:17:55 +0100

    Included wrapper method to suspend/resume a given domain

--- libvirt_wrapper.py
+++ libvirt_wrapper.py
@@ -15,19 +15,25 @@ class LibvirtWrapper:
         self.connections = dict()
         self.domains = dict()
 
+    def connect_local(self):
+        self.connect_host('localhost', 'qemu+tcp://localhost/system')
+
     def connect_host(self, host, url):
         try:
-            self.connections[host] = libvirt.openReadOnly(url)
+            self.connections[host] = libvirt.open(url)
         except libvirt.libvirtError:
             sys.stdout.write("Failed to establish connection to host %s with url %s" % (host, url))
             sys.stdout.flush()
 
-    def get_info(self, host, domain):
+    def _check_host(self, host):
         if host not in self.connections:
             sys.stdout.write("Host not connected. Please invoke connect_host first.")
             sys.stdout.flush()
-            return
+            return False
+        else:
+            return True
 
+    def _lookup_domain(self, host, domain):
         if domain not in self.domains:
             try:
                 self.domains[domain] = self.connections[host].lookupByName(domain)
@@ -35,5 +41,32 @@ class LibvirtWrapper:
                 sys.stdout.write("Failed to get_info for host with domain %s" % domain)
                 sys.stdout.flush()
 
+    def get_info(self, host, domain):
+        if not self._check_host(host):
+            return False
+
+        # lookup the domain
+        self._lookup_domain(host, domain)
+
         # Get info from the libvirt domain
         return self.domains[domain].info()
+
+    def suspend(self, host, domain):
+        if not self._check_host(host):
+            return False
+
+        # lookup the domain
+        self._lookup_domain(host, domain)
+
+        # suspend the domain
+        return self.domains[domain].suspend()
+
+    def resume(self, host, domain):
+        if not self._check_host(host):
+            return False
+
+        # lookup the domain
+        self._lookup_domain(host, domain)
+
+        # resume the domain
+        return self.domains[domain].resume()
